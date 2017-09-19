b3e4f004e8ff4ffc92d5f1c010e6639cb85f0850
chxgo <santiago.lunar.m@gmail.com>
2017-09-19 15:53:55 -0300

    Minor fixes

--- logs-order-1.2.sh
+++ logs-order-1.2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Ordering & backing up Docker Container's Logs
-# Writter by Santiago Lunar
+# Written by Santiago Lunar
 # santiagolunar@protonmail.com
 
 # Define variables
 log_path="/var/log/containers"
 backup_path="/storage/docker/backups"						# Root directory
 log_collect_path="/storage/docker/logs"						# Log's collecting dirs
-dirs=$(ls $log_path)										# Directories to iterate	
-today="`date +%Y%m%d`"										# 
-aws_bucket="s3://slunarcrossover"							# AWS S3 Bucket 
+dirs=$(ls $log_path)										# Directories to iterate
+today="`date +%Y%m%d`"										#
+aws_bucket="s3://slunarcrossover"							# AWS S3 Bucket
 aws_opts_l="--recursive --exclude '*' --include '*.log'"	# AWS options for Logs uploading
 aws_opts_b="--recursive --exclude '*' --include '*.bz2'"	# AWS options for Backups uploading
 
@@ -23,7 +23,7 @@ for i in $dirs; do
 			echo ""
 			echo "Directory $log_collect_path/$i/$today successfully created"
 			echo ""
-		else 
+		else
 			echo "ERROR: Directory not created"
 			echo "TERMINATED WITH ERRORS"
 	fi
@@ -47,7 +47,7 @@ echo ""
 echo "Compressing Logs to .bz2 and moving them to Backups folder"
 for i in $dirs; do
 	if pbzip2 -kv -p2 $log_collect_path/$i/$today/*.log
-		then 
+		then
 			echo "Data compressed successfully"
 			echo "Moving to backup folder..."
 			if mv $log_collect_path/$i/$today/*.bz2 $backup_path/$i/$today
