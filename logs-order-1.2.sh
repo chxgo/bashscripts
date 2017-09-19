98e377c911b64c17e7dd1c16b6c1e9e854e839c1
chxgo <santiago.lunar.m@gmail.com>
2017-09-19 16:43:37 -0300

    Echo substitution for printf

--- logs-order-1.2.sh
+++ logs-order-1.2.sh
@@ -14,61 +14,61 @@ aws_opts_l="--recursive --exclude '*' --include '*.log'"	# AWS options for Logs 
 aws_opts_b="--recursive --exclude '*' --include '*.bz2'"	# AWS options for Backups uploading
 
 # Create directories with actual date
-echo "Creating date directories within $backup_path and $log_collect_path"
+printf "Creating date directories within $backup_path and $log_collect_path"
 for i in $dirs; do
 	if mkdir -p $backup_path/$i/$today && mkdir -p $log_collect_path/$i/$today
 		then
-			echo ""
-			echo "Directory $backup_path/$i/$today successfully created"
-			echo ""
-			echo "Directory $log_collect_path/$i/$today successfully created"
-			echo ""
+			printf ""
+			printf "Directory $backup_path/$i/$today successfully created"
+			printf ""
+			printf "Directory $log_collect_path/$i/$today successfully created"
+			printf ""
 		else
-			echo "ERROR: Directory not created"
-			echo "TERMINATED WITH ERRORS"
+			printf "ERROR: Directory not created"
+			printf "TERMINATED WITH ERRORS"
 	fi
 done
 
 # Find logs and copy them from logs dirs to Collect Log dirs
-echo "Finding all Logs and copying them to Collect Log Dirs"
+printf "Finding all Logs and copying them to Collect Log Dirs"
 for i in $dirs; do
 	if find $log_path/$i -maxdepth 1 -name *log  -exec cp -t $log_collect_path/$i/$today {} \;
 		then
-			echo ""
-			echo "Log files were successfully copied to $i Collect folder"
-			echo ""
+			printf ""
+			printf "Log files were successfully copied to $i Collect folder"
+			printf ""
 		else
-			echo "ERROR No files were copied to Collect folder"
+			printf "ERROR No files were copied to Collect folder"
 	fi
 done
 
 # Compress moved data into .bz2
-echo ""
-echo "Compressing Logs to .bz2 and moving them to Backups folder"
+printf ""
+printf "Compressing Logs to .bz2 and moving them to Backups folder"
 for i in $dirs; do
 	if pbzip2 -kv -p2 $log_collect_path/$i/$today/*.log
 		then
-			echo "Data compressed successfully"
-			echo "Moving to backup folder..."
+			printf "Data compressed successfully"
+			printf "Moving to backup folder..."
 			if mv $log_collect_path/$i/$today/*.bz2 $backup_path/$i/$today
 				then
-					echo "Data moved successfully to $backup_path/$i/$today"
+					printf "Data moved successfully to $backup_path/$i/$today"
 				else
-					echo "ERROR: Data NOT moved to backup folder"
+					printf "ERROR: Data NOT moved to backup folder"
 			fi
 		else
-			echo "ERROR: Data NOT compressed"
-			echo "TERMINATED WITH ERRORS"
+			printf "ERROR: Data NOT compressed"
+			printf "TERMINATED WITH ERRORS"
 	fi
 done
 
 # Sending Logs and crompressed backups to S3 bucket
-echo "Sending Log files and compressed backups to AWS S3 bucket"
+printf "Sending Log files and compressed backups to AWS S3 bucket"
  for i in $dirs; do
  	if aws s3 cp $log_collect_path/$i/$today $aws_bucket/logs/$i/$today $aws_opts_l && aws s3 cp $backup_path/$i/$today $aws_bucket/backups/$i/$today $aws_opts_b
  		then
- 			echo "Now your logs and backups are in AWS S3"
+ 			printf "Now your logs and backups are in AWS S3"
  		else
- 			echo "ERROR: Your data is NOT in AWS S3"
+ 			printf "ERROR: Your data is NOT in AWS S3"
  	fi
  done
