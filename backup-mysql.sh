6095ce27d2bfe60dfb6c68de083f303b25bacb59
chxgo <santiago.lunar.m@gmail.com>
2017-09-19 19:40:23 -0300

    Improved commentaries

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-# Ordering & backing up Docker Container's Logs
+# Backup each database to gzip files
 # Written by Santiago Lunar
 # santiagolunar@protonmail.com
+
+# First off: security!
 
 set -o errexit												# Exit when a command fails
 set -o nounset												# Exit when trying to use undeclared variables.
 
-host="10.0.0.213"
-dbDir="/media/backup1/srv213/mysql/"
-
-dumpOpts="--skip-extended-insert --force --lock-all-tables"
-mysql="/usr/bin/mysql"
-
+# Define variables
+host="10.0.0.213"                                           # The host where the databases are
+dbDir="/media/backup1/srv213/mysql/"                        # Folder to allocate compressed backups
+dumpOpts="--skip-extended-insert --force --lock-all-tables" # Options for mysqldump
+mysql="/usr/bin/mysql"                                      # MySQL executable path
 today=$(date "+%d-%b-%Y")
 
-# Obtener una lista de las bases de datos
-printf "Obtener una lista completa de las bases de datos\n"
-dbs=$(echo "SHOW DATABASES" | $mysql -h $host -u respaldos)
+# Let's begin
+printf "Obteniendo una lista completa de las bases de datos\n"
+dbs=$(echo "SHOW DATABASES" | $mysql -h $host -u respaldos) # Get a complete list of DBs
 
-
+printf "Empieza el respaldo de "
 for i in $dbs; do
-        date=$today
-        file="$dbDir/$today/$i.sql.gz"
-        printf "Backing up '$i' from '$host' on '$date' to:\n"
-        printf "$file\n"
-        mysqldump $dumpOpts -h $Host $db | gzip > $file
+    date=$today
+    file="$dbDir/$today/$i.sql.gz"
+    printf "Backing up '$i' from '$host' on '$date' to:\n"
+    printf "$file\n"
+    mysqldump $dumpOpts -h $Host $db | gzip > $file
 done
 exit
