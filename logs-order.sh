cdfa31cbe6c72f7b68b58ad6724ce0aabdc0fe85
chxgo <santiago.lunar.m@gmail.com>
2017-09-19 22:25:37 -0300

    Fixed long lines

--- logs-order.sh
+++ logs-order.sh
@@ -18,9 +18,11 @@ awsOptsBcks=(--recursive --exclude '*' --include '*.bz2')	# Options fot Backups 
 printf "Creating directories with date... \n"
 for i in "${dirs[@]}"; do
 		if mkdir -p $dockDir/logs/"$i"/"$today" && mkdir -p $dockDir/backups/"$i"/"$today"; then
-				printf "Folders %s/logs/%s/%s and $dockDir/backups/$i/$today successfully created\n" "$dockDir" "$i" "$today"
+				printf "Folders %s/logs/%s/%s and $dockDir/backups/$i/$today \
+				successfully created\n" "$dockDir" "$i" "$today"
 			else
-				printf "ERROR: Directory not created"; printf "TERMINATED WITH ERRORS\n"
+				printf "ERROR: Directory not created\n"
+				printf "TERMINATED WITH ERRORS\n"
 		fi
 	done
 
@@ -55,7 +57,9 @@ done
 # Sending Logs and backups to AWS S3 Bucket
 printf "\n"; printf "Uploading Collected Logs and *.bz2 Backups to AWS S3 Bucket\n"
 for i in "${dirs[@]}"; do
-	if aws s3 mv $dockDir/logs/"$i"/"$today" s3://slunarcrossover/logs/"$i"/"$today" "${awsOptsLogs[@]}" && aws s3 mv $dockDir/backups/"$i"/"$today" s3://slunarcrossover/backups/"$i"/"$today" "${awsOptsBcks[@]}"; then
+	if aws s3 mv $dockDir/logs/"$i"/"$today" s3://slunarcrossover/logs/"$i"/"$today" \
+	"${awsOptsLogs[@]}" && aws s3 mv $dockDir/backups/"$i"/"$today" s3://slunarcrossover/backups/"$i"/"$today" \
+	"${awsOptsBcks[@]}"; then
 			printf "Now your logs are in AWS S3\n"
 		else
 			printf "ERROR: Your data is NOT in AWS S3\n"
