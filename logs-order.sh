7d5c3d126f23093b7a944b9993935fc4581eeef4
chxgo <santiago.lunar.m@gmail.com>
2017-09-19 21:42:07 -0300

    Shellchecked!

--- logs-order.sh
+++ logs-order.sh
@@ -11,9 +11,8 @@ logDir="/var/log/containers" 								# Syslog container's folder
 dockDir="/storage/docker"									# Backups folder
 dirs=("mysql" "httpd")											# List of directories for loop iteration
 today=$(date +%Y%m%d)										#
-awsOptsLogs="--recursive --exclude '*' --include '*.log'"	# Options for Logs folder to AWS S3
-awsOptsBcks="--recursive --exclude '*' --include '*.bz2'"	# Options fot Backups folder to AWS S3
-
+awsOptsLogs=(--recursive --exclude '*' --include '*.log')	# Options for Logs folder to AWS S3
+awsOptsBcks=(--recursive --exclude '*' --include '*.bz2')	# Options fot Backups folder to AWS S3
 
 # Create date directories under backups and logs collecting folder
 printf "Creating directories with date... \n"
@@ -28,13 +27,13 @@ for i in "${dirs[@]}"; do
 # Find logs and copy them from logs folder to Log collecting folder
 printf "Finding Container's Logs and copying them to Collect folder and Backups folder\n"
 for i in "${dirs[@]}"; do
-	if find "$logDir"/"$i" -maxdepth 1 -name *log  -exec cp --target-directory="$dockDir/logs/$i/$today" {} \;
+	if find "$logDir"/"$i" -maxdepth 1 -name '*log' -exec cp --target-directory="$dockDir/logs/$i/$today" {} \;
 		then
 			printf "\n"; printf "Logs have been copied to Log's collecting folder\n"; printf "\n"
 		else
 			printf "\n"; printf "ERROR: Logs haven't been copied\n"; printf "\n"
 	fi
-	if find "$logDir"/"$i" -maxdepth 1 -name *log  -exec cp --target-directory="$dockDir/backups/$i/$today" {} \;
+	if find "$logDir"/"$i" -maxdepth 1 -name '*log'  -exec cp --target-directory="$dockDir/backups/$i/$today" {} \;
 		then
 			printf "\n"; printf "Logs have been copied to Backups folder\n"; printf "\n"
 		else
@@ -56,7 +55,7 @@ done
 # Sending Logs and backups to AWS S3 Bucket
 printf "\n"; printf "Uploading Collected Logs and *.bz2 Backups to AWS S3 Bucket\n"
 for i in "${dirs[@]}"; do
-	if aws s3 mv $dockDir/logs/"$i"/"$today" s3://slunarcrossover/logs/"$i"/"$today" $awsOptsLogs && aws s3 mv $dockDir/backups/"$i"/"$today" s3://slunarcrossover/backups/"$i"/"$today" $awsOptsBcks; then
+	if aws s3 mv $dockDir/logs/"$i"/"$today" s3://slunarcrossover/logs/"$i"/"$today" "${awsOptsLogs[@]}" && aws s3 mv $dockDir/backups/"$i"/"$today" s3://slunarcrossover/backups/"$i"/"$today" "${awsOptsBcks[@]}"; then
 			printf "Now your logs are in AWS S3\n"
 		else
 			printf "ERROR: Your data is NOT in AWS S3\n"
