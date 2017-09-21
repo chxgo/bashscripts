6348603e455ba6aeb94943f99700d0926770b38a
chxgo <santiago.lunar.m@gmail.com>
2017-09-21 15:53:45 -0300

    Exercise 3-1

--- perl/ex3-1.pl
+++ perl/ex3-1.pl
@@ -2,3 +2,14 @@
 # Exercise 3-1
 # Write a program that reads a list of strings on separate lines until end-ofinput
 # and prints out the list in reverse order.
+
+my @completeName = qw/ Santiago Rafael Lunar Millan /;
+print "Your complete name is @completeName\n\n";
+my @sortCompName = sort @completeName;
+my @revCompName = reverse @completeName;
+print "Your reverse name is @revCompName\n\n";
+print "And your sorted name is @sortCompName\n\n";
+my $revCompName = reverse @completeName;
+print "...and your fully reverse name is $revCompName\n\n";
+my $revCompName = @completeName;
+print "...or $revCompName \n\n"
