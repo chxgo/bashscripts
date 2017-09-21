4e11c8ffe7e38f2759c8ca2e4168b0c5786a9952
chxgo <santiago.lunar.m@gmail.com>
2017-09-21 18:15:31 -0300

    Disable strict

--- perl/ex-2-4.pl
+++ perl/ex-2-4.pl
@@ -3,12 +3,12 @@
 # Write a program that prompts for and reads two numbers (on separate lines
 # of input) and prints out the product of the two numbers multiplied together.
 
-use strict;
+#use strict;
 use warnings;
 
 print "Please enter first value: \n";
-chomp (my $num1 = <STDIN>);
+chomp ($num1 = <STDIN>);
 print "Please enter second value: \n";
-chomp (my $num2 = <STDIN>);
-my $num3 = $num1*$num2;
+chomp ($num2 = <STDIN>);
+$num3 = $num1*$num2;
 print "The product of first and second values is $num3\n\n";
