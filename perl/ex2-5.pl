8a115a02e3aec9a7d9335969a4e6ebac0be72778
chxgo <santiago.lunar.m@gmail.com>
2017-09-21 18:14:12 -0300

    Disable strict

--- perl/ex2-5.pl
+++ perl/ex2-5.pl
@@ -6,12 +6,12 @@
 # “3”, the output should be three lines, each saying “fred”. If the user enters “fred”
 # and “299792,” there may be a lot of output.
 
-use strict;
+#use strict;
 use warnings;
 print "Please enter your name: \n";
-chomp (my $name = <STDIN>);
+chomp ($name = <STDIN>);
 print "Now, enter your age: \n";
-chomp (my $age = <STDIN>);
+chomp ($age = <STDIN>);
 #while ($age gt 0) {
 #    print "$age Your name is $name \n";
 #    $age -= 1;
