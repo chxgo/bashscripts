349078c175f98f499cf3ec197db8755206687818
chxgo <santiago.lunar.m@gmail.com>
2017-09-21 11:57:57 -0300

    Exercise 2.5

--- perl/ex2-5.pl
+++ perl/ex2-5.pl
@@ -5,3 +5,14 @@
 # number on separate lines. (Hint: use the x operator.) If the user enters “fred” and
 # “3”, the output should be three lines, each saying “fred”. If the user enters “fred”
 # and “299792,” there may be a lot of output.
+
+use strict;
+use warnings;
+print "Please enter your name: \n";
+chomp (my $name = <STDIN>);
+print "Now, enter your age: \n";
+chomp (my $age = <STDIN>);
+while ($age gt 0) {
+    print "$age Your name is $name \n";
+    $age -= 1;
+}
