789a2319fc45331b7f97180097ec70ea9c96740a
chxgo <santiago.lunar.m@gmail.com>
2017-09-20 21:13:13 -0300

    Whirlwind

--- perl/whirlwind.pl
+++ perl/whirlwind.pl
@@ -1,13 +1,8 @@
 #!/usr/bin/perl
 # Created by Santiago Lunar from Learning Perl book
-<<<<<<< HEAD
-@lines = `perldoc -u -f atan2`;
-foreach (@lines) {
-=======
-#!/usr/bin/perl
+
 @lines = `perldoc -u -f atan2`;
   foreach (@lines) {
->>>>>>> 9cafa68b12d2db7552bea36c8338e330f7da6330
   s/\w<([^>]+)>/\U$1/g;
   print;
 }
