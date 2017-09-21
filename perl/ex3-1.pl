28ca84d14a0164078970559785bf5ab6ea68d40e
chxgo <santiago.lunar.m@gmail.com>
2017-09-21 18:13:37 -0300

    Disable strict

--- perl/ex3-1.pl
+++ perl/ex3-1.pl
@@ -5,13 +5,13 @@
 
 print "Please enter your complete name: \n";
 #my @realName = chomp (my @realName = <STDIN>);
-my @realName = qw/ Santiago Lunar Millan/;
+@realName = qw/ Santiago Lunar Millan/;
 print "Your complete name is @realName\n\n";
-my @sortCompName = sort (@realName);
-my @revCompName = reverse (@realName);
+@sortCompName = sort (@realName);
+@revCompName = reverse (@realName);
 print "Your reverse name is @revCompName\n\n";
 print "And your sorted name is @sortCompName\n\n";
-my $revCompName = reverse (@realName);
+$revCompName = reverse (@realName);
 print "...and your fully reverse name is $revCompName\n\n";
-my $revCompName = @realName;
+$revCompName = @realName;
 print "...and it has $revCompName silabes on it.\n\n"
