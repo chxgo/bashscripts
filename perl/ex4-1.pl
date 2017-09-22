d7d62d6c035f9bf004db35021cf2006dabac3eec
chxgo <santiago.lunar.m@gmail.com>
2017-09-22 14:18:50 -0300

    Exercise 4-1

---
+++ perl/ex4-1.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+# Exercise 4-1
+# Write a subroutine, named total, which returns the total of a list of num‐ bers.
+# Hint: the subroutine should not perform any I/O; it should simply process its parameters and
+# return a value to its caller. Try it out in this sample program, which merely exercises
+# the subroutine to see that it works. The first group of numbers should add up to 25.
