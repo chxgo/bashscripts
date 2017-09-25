df0f1b3fa4f8de3017cd698c4dfacb2148e8db46
chxgo <santiago.lunar.m@gmail.com>
2017-09-25 15:54:17 -0300

    Exercise 5-2

--- perl/ex5-2.pl
+++ perl/ex5-2.pl
@@ -7,7 +7,7 @@ use autodie;
 
 printf "Write some words. After each word press \"Enter\" and when finished typing press Ctrl\+D:\n";
 chomp (my @lines = <STDIN>);
-printf "Please chose a number for column width\n";
+printf "Please choose a number for column width: \n";
 chomp (my $colWidth = <STDIN>);
-print "1234567890" x 7, "12345\n"; # ruler line to column 75
-printf "This is the text you just typed in:\n\n" . ("%*s\n" x @lines), $colWidth, @lines;
+printf "1234567890" x 7, "12345\n"; # ruler line to column 75
+printf "This is the text you just typed in:\n\n" . ("%${colWidth}s\n" x @lines), @lines;
